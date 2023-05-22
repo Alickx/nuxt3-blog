e9eaec3b15fadfa046cf04818adcc2639e373b7a
Alickx <alickx@foxmail.com>
2023-05-22 20:35:13 +0800

    feat: 增加返回按钮

--- src/components/ArticleHeader.vue
+++ src/components/ArticleHeader.vue
@@ -2,7 +2,7 @@
   <div class="relative bg-[#1a316b] p-6">
     <div class="w-5/6 mx-auto flex flex-row justify-between">
       <div class="flex flex-row space-x-5">
-        <Icon name="material-symbols:chevron-left-rounded" color="white" size="28" />
+        <Icon class="cursor-pointer" @click="$router.go(-1)" name="material-symbols:chevron-left-rounded" color="white" size="28" />
         <Icon name="material-symbols:menu" color="white" size="28" />
         <span class="text-white font-black text-xl">平平凡凡的Alick</span>
       </div>
