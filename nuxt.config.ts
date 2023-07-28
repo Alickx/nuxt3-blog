147bf6916d1d1d2114b69b391ee14c7a1895fd3b
alickx <alickx@foxmail.com>
2023-07-29 02:08:16 +0800

    feat: 暂时提交，准备改为纯静态本地读取md文件展示，不做db存储查询

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon','@vueuse/nuxt',],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon','@vueuse/nuxt','@ant-design-vue/nuxt'],
   css: ['@/assets/sytles/normalize.css'],
   srcDir: 'src/',
 })
