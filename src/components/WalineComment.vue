cbb9dc05a1cef78934a5ae401c91a4fdc8c79c7e
Alickx <caiguopeng@ama-auto.com>
2023-12-04 18:44:01 +0800

    update: 修改评论深色模式

--- src/components/WalineComment.vue
+++ src/components/WalineComment.vue
@@ -1,9 +1,10 @@
 <template>
-  <Waline :serverURL="serverURL" :path="path" />
+  <Waline :serverURL="serverURL" :path="path" data='html[data-theme="dark"]' />
 </template>
 <script setup>
 import { Waline } from "@waline/client/component";
 import "@waline/client/dist/waline.css";
+import '@waline/client/waline-meta.css';
 
 const serverURL = "https://comment.alickx.top";
 const path = computed(() => useRoute().path);
