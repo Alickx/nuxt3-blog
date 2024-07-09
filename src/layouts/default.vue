c0942498d372ba053ab7dd48d3c93de893fca3c2
caiguopeng <caiguopeng@ama-auto.com>
2024-07-09 18:25:00 +0800

    chore: 添加 @vercel/speed-insights 依赖并在默认布局中引入 SpeedInsights 组件

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <SpeedInsights />
   <div class="flex min-h-screen flex-col">
     <Header />
     <div class="flex-1 bg-white dark:bg-[#212526]">
@@ -8,4 +9,6 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
