3864778f74af0b17e0627812aff702a5356d2a07
Alickx <alickx@foxmail.com>
2024-09-18 22:54:41 +0800

    修改个人介绍以及去除vercel的速度检测组件

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,4 @@
 <template>
-  <SpeedInsights />
   <div class="flex min-h-screen flex-col">
     <Header />
     <div class="flex-1 bg-white dark:bg-[#212526]">
@@ -10,5 +9,4 @@
 </template>
 
 <script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
